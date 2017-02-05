8f667d5dae512ab2d0498a376781bb7e8656741f
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-02-05 16:08:22 +0100

    Forgot to add minmax

--- minimax.py
+++ minimax.py
@@ -1,1 +1,37 @@
+import copy
 
+class AI:
+
+    def minimax(self, gameState, depth, maximizingplayer = True):
+
+        if (depth == 0 or len(gameState.validMoves()) == 0):
+            return {'move': None, 'value': gameState.stateValue()}
+
+
+        if maximizingplayer:
+            moves = []
+            for move in gameState.validMoves():
+                newGameState = copy.deepcopy(gameState)
+                newGameState.placeDisk(move)
+                value = self.minimax(newGameState, depth-1, False)['value']
+                moveAndValue = {'move': move,'value': value}
+                moves.append(moveAndValue)
+            bestMove = {'value': -1000000}
+            for moveAndValue in moves:
+                if moveAndValue['value'] > bestMove['value']:
+                    bestMove = moveAndValue
+            return bestMove
+
+        else:
+            moves = []
+            for move in gameState.validMoves():
+                newGameState = copy.deepcopy(gameState)
+                newGameState.placeDisk(move)
+                value = self.minimax(newGameState, depth-1, True)['value']
+                moveAndValue = {'move': move,'value': value}
+                moves.append(moveAndValue)
+            bestMove = {'value': 1000000}
+            for moveAndValue in moves:
+                if moveAndValue['value'] < bestMove['value']:
+                    bestMove = moveAndValue
+            return bestMove
