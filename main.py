6a79cc430150b6a8a65c7f043eac858086cf9b6d
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-02-05 16:08:57 +0100

    and main ...

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 from game import GameState
-import minimax
+from move import Move
+from minimax import AI
+import copy
 
 
 game = GameState()
 game.initialize()
-game.printGameState()
 while (True):
-    print ('')
     coordinates = [1]
     while(len(coordinates) != 2):
+        print ("")
         if (game.getPlayerOneTurn()):
+            game.printGameState()
+            coordinates = input('Give me your coordinates: \n').split(' ')
             print ('Player W')
+            x = int(coordinates[0]) - 1
+            y = int(coordinates[1]) - 1
+            game.placeDisk(Move(x, y))
         else:
-            print ('Player B')
-        coordinates = input('Give me your coordinates: \n').split(' ')
+            #print ('Player B')
+            ai = AI()
+            aiGame = copy.deepcopy(game)
+            aiGame.suppresPrint()
+            action = ai.minimax(aiGame, 5)
+            game.placeDisk(action['move'])
 
-    x = int(coordinates[0]) - 1
-    y = int(coordinates[1]) - 1
-    game.placeDisk(x, y)
-    game.printGameState()
-    if(game.checkWin()):
-        if(game.getPlayerOneTurn()):
-            print ('Winner is: Player B')
+            print ('Comupter placed in: ' + str(action['move'].getX() + 1) + ' ' + str(action['move'].getY() + 1))
 
+
+
+
+
+    if (len(game.validMoves()) == 0):
+        if (game.stateValue() > 0):
+            print ('Human Won!!')
+        elif(game.stateValue() < 0):
+            print ('Computer Won!!')
         else:
-            print ('Winner is: Player W')
-        break
+            print ('Its a draw!')
